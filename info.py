8852d7911bbd4053bcb42533a46e8237c2937d9c
sky-creations <121612750+sky-botz@users.noreply.github.com>
2023-02-08 19:56:29 +0530

    Update info.py

--- info.py
+++ info.py
@@ -40,7 +40,7 @@ FILDLT_CNL = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "")
-DATABASE_NAME = environ.get('DATABASE_NAME', "EvaMaria")
+DATABASE_NAME = environ.get('DATABASE_NAME', "projections")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Channel Button Links
